e2658ce18a0c941e98a58be1f1554a3b3e46f3eb
Matthias Humt <matthias.humt@dlr.de>
2021-04-21 09:48:58 +0200

    Name change

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import find_packages, setup
 
-setup(name="rmc_open3d",
+setup(name="easy-o3d",
       version="0.1",
       packages=find_packages(exclude=["tests", "*.tests", "*.tests.*", "tests.*"]),
       description="Uses Open3D functionality to implement various 3D data based algorithm used at RMC",
--- tests/context.py
+++ tests/context.py
@@ -2,4 +2,4 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-from rmc_open3d import utils, icp, interfaces
+from easy_o3d import utils, icp, interfaces
