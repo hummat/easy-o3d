c9dea4ff2805155825a11783d06878eb07681df3
Matthias Humt <matthias.humt@dlr.de>
2021-04-21 12:02:58 +0200

    Docu

---
+++ scripts/hyperopt.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Performs hyperparameter optimization for the registration algorithms from this package using Scikit Optimize."""
+import skopt
+
+
+if __name__ == "__main__":
+    pass
---
+++ scripts/run_registration.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Performs point cloud registration using registration algorithms from this package."""
+from easy_o3d import utils, registration
+
+
+if __name__ == "__main__":
+    pass
